c6b646a530b54e784ecb09b39ed70f67ef39febf
Jimmy <jimmyjamesbaldwin@gmail.com>
2014-04-23 14:36:47 +0100

    Removed incorrect master brand

--- loaned-android-master/src/com/mattallen/loaned/history/LoanHistoryFragment.java
+++
@@ -1,8 +0,0 @@
-package com.mattallen.loaned.history;
-
-import android.support.v4.app.Fragment;
-
-public abstract class LoanHistoryFragment extends Fragment {
-
-	public abstract void updateInformation();
-}
